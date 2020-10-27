2bfa28126f972f52d357587210d2daa065ed7004
Marcelo Oliveira <marcelo_alfahot@hotmail.com>
2020-10-26 23:30:28 -0300

    CORS in main

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/aws/aws-sdk-go v1.35.7
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gofiber/cors v0.2.2
 	github.com/gofiber/fiber v1.14.6
 	github.com/gofrs/uuid v3.2.0+incompatible
 	github.com/joho/godotenv v1.3.0
--- main.go
+++ main.go
@@ -6,12 +6,14 @@ import (
 	m "github.com/ecommerce/db"
 	r "github.com/ecommerce/routes"
 	u "github.com/ecommerce/utility"
+	"github.com/gofiber/cors"
 	"github.com/gofiber/fiber"
 )
 
 func main()  {
 	m.CreateConnection()
 	app := fiber.New()
+	app.Use(cors.New())
 	r.Routes(app)
 	port := u.GetDotEnv("PORT")
 
